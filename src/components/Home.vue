855584e8c26dc5d29591a2a816e14c4bcedf79fb
Staffan <staffan@wallmanderco.se>
2018-11-21 15:26:56 +0100

    Fixed lint error

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,11 @@
         <p id="text" v-show="isShown">Lorem ipsum dolor sit amet...</p>
         <div class="product-container">
             <ul>
-                <li v-for="product in products">{{ product.name }}</li>
+                <li v-for="product in products"
+                  :key="product.name"
+                >
+                  {{ product.name }}
+                </li>
             </ul>
         </div>
     </div>
